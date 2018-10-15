071cdf79831fd7fd6ab0cdea5927e010660c1253
btoner <ben.toner@wework.com>
2018-10-15 18:02:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const API_KEY = "fea3ae9cd39e9e9a15170ade49a4c4ab"
 
 function handleFormSubmit(event) {
   //handle submit event
+  
+  
   fetch('api.openweathermap.org/data/2.5/weather?q={city name}').then
 }
 
